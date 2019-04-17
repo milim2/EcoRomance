d65949cc4554bf7e59a925dd365ea91f78daff94
milimlee <milimlee0@gmail.com>
2019-04-16 21:31:18 -0400

    Add files via upload

---
+++ ProductDetails.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Home.Master" AutoEventWireup="true" CodeBehind="ProductDetails.aspx.cs" Inherits="Project5.ProductDetail" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style type="text/css">
+        .auto-style8 {
+            width: 100%;
+        }
+        .auto-style9 {
+            width: 117px;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:DataList ID="dlproduct" runat="server" RepeatColumns="1">
+        <ItemTemplate>
+            <table class="auto-style8">
+                <tr>
+                    <td class="auto-style9">
+                        <asp:Image ID="Image1" runat="server" Height="119px" ImageUrl='<%# string.Format("~/{0}",Eval("ProductImage"))%>' />
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td class="auto-style9">Name</td>
+                    <td>
+                        <asp:Label ID="Label1" runat="server" Text='<%# Eval("ProductName") %>'></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style9">Price</td>
+                    <td>
+                        <asp:Label ID="Label2" runat="server" Text='<%# Eval("ProductPrice") %>'></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style9">Description</td>
+                    <td>
+                        <asp:Label ID="Label3" runat="server" Text='<%# Eval("Description") %>'></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style9">&nbsp;</td>
+                    <td>
+                        <asp:Button ID="btnaddtocart" runat="server" Text="Add to Cart" OnClick="btnaddtocart_Click" />
+                    </td>
+                </tr>
+            </table>
+        </ItemTemplate>
+    </asp:DataList>
+</asp:Content>
---
+++ ProductDetails.aspx.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project5
+{
+    public partial class ProductDetail : System.Web.UI.Page
+    {
+        string cs = "Data Source =(local); Initial Catalog = ShoppingMall; Integrated Security = True";
+
+        ShoppingCart shoppingcart;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    try
+                    {
+                        string pid = Request.QueryString["ProductID"];
+
+                        if (pid != null)
+                        {
+                            string query = "Select * from Product where ProductID =" + pid;
+                            con.Open();
+                            SqlCommand cmd = new SqlCommand(query, con);
+                            DataSet ds = new DataSet();
+                            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+
+                            sda.Fill(ds);
+                            if (ds.Tables[0].Rows.Count > 0)
+                            {
+                                dlproduct.DataSourceID = null;
+                                dlproduct.DataSource = ds;
+                                dlproduct.DataBind();
+                            }
+
+                            sda.Dispose();
+                        }
+
+
+                        /*
+                        SqlDataReader rdr = cmd.ExecuteReader();
+                        DataTable dt = new DataTable();
+                        
+                        dlmenu.DataSource = dt;
+                        dlmenu.DataBind();
+                        rdr.Close(); */
+                    }
+                    catch (Exception ex)
+                    {
+                        ex.Message.ToString();
+
+                    }
+                    finally
+                    {
+
+                        con.Close();
+                    }
+                }
+
+            }
+
+        }
+
+        protected void btnaddtocart_Click(object sender, EventArgs e)
+        {
+            if(Session["CART"] == null)
+            {
+                shoppingcart = new ShoppingCart();
+                Session["CART"] = shoppingcart;    
+            }
+            
+                shoppingcart = (ShoppingCart)Session["CART"];
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                try
+                {
+                    string pid = Request.QueryString["ProductID"];
+
+                  
+                        string query = "Select * from Product where ProductID =" + pid;
+                        con.Open();
+                        SqlCommand cmd = new SqlCommand(query, con);
+                        DataTable dt = new DataTable();
+
+                    DataRow row = dt.Rows[0];
+                    shoppingcart.Insert(new CartItem(Int32.Parse(pid),
+                                   row["ProductName"].ToString(), 
+                                   row["ProductImage"].ToString(),
+                                   row["Description"].ToString(), 
+                                   Int32.Parse(row["ProductPrice"].ToString()), 1));                    
+                }
+                catch (Exception ex)
+                {
+                    ex.Message.ToString();
+
+                }
+                finally
+                {
+
+                    con.Close();
+                }
+            }
+
+        }
+
+
+    }
+}
